931320d3a294558a1038da76744922ff3fe70437
SiobanL <limosanisioban@gmail.com>
2022-11-12 23:45:09 +0100

    maj

--- MyClass.py
+++ MyClass.py
@@ -10,13 +10,11 @@ class Book:
         self.bookID: int = bookID
     
     def __str__(self) -> str:
-        bookTitlestr = self.bookTitle
-        bookAuthorstr = self.bookAuthor
-        bookEditionstr = self.bookEdition
-        bookISBNstr = str(self.bookISBN)
-        bookIDstr = str(self.bookID)
-        return bookTitlestr +":"+ bookAuthorstr +":"+ bookEditionstr +":"+ bookISBNstr +":"+ bookIDstr
-
+        return self.bookTitle + ";" + self.bookAuthor + ";" + self.bookEdition + ";" + str(self.bookISBN) + ";" + str(self.bookID) + "\n"
+    
+    def __repr__(self) -> str:
+        return f"The title of book is {self.bookTitle} the author is {self.bookAuthor} and it is part of the edition {self.bookEdition} the ISBN is {self.bookISBN}\n"
+    
     def get_bookTitle(self)-> str:
         return self.bookTitle
     
@@ -32,30 +30,48 @@ class Book:
     def get_bookID(self)-> int:
         return self.bookID
     
+    def changeTitle(self,newTitle:str)-> None:
+        self.bookTitle = newTitle
+        
+    def changeAuthor(self,newAuthor: str)-> None:
+        self.bookAuthor = newAuthor
+
+    def changeEdition(self,newEdition: str)-> None:
+        self.bookEdition = newEdition
+
+    def changeISBN(self,newISBN:int)-> None:
+        self.bookISBN = newISBN
+    
     
 class myFile:
     """
-        Class to manage db file of books
+        Class to manage db file of books in a typed file as -.si.
     """
-    import os
+    import os.path
     
     def __init__(self,FileName) -> None:
         self.FileName = FileName
 
-    def createOrLoadFile(self):
-        if self.os.path.exists(self.FileName):
-            with open(self.FileName,"r+") as f:
-                rlist:list = []
-                for line in f:
-                    rlist.append(line)
-                f.close()
-            return rlist
-        else:
-            file = open(self.FileName,"x")
-            file.close()
-            return []
-
-    def saveInFile(self,info):
+    def createFile(self)-> None:
+        f = open(self.FileName,"x")
+        f.close()
+    
+    def saveInFile(self,info:str)-> None:
         with open(self.FileName,"a+") as f:
             f.write(info)
             f.close()
+
+    def loadToFile(self)-> list:
+        with open(self.FileName,"r+")as f:
+            rlst: list = []
+            for line in f:
+                rlst.append(line)
+            f.close()
+        return rlst
+    
+    def createOrLoadFile(self)-> list:
+        if self.os.path.exists(self.FileName):
+            return self.loadToFile()
+        else:
+            self.createFile()
+            return []
--- main.py
+++ main.py
@@ -2,15 +2,56 @@ from MyClass import Book, myFile as file
 
 
 programAuthor: str = "Sioban L." + chr(169)
-book = Book("Title 1","Author 1","Edition 1",11,1)
+book1 = Book("Title 1","Author 1","Edition 1",11,1)
+book2 = Book("Title 2","Author 2","Edition 2",22,2)
+book3 = Book("Title 3","Author 3","Edition 3",33,3)
+f = file("Book.si")
+
+def display():
+    while True:
+        print("+--------------------------+")
+        print("|   a: Ajouter un livre    |")
+        print("|   b: Preter un livre     |")
+        print("|   c: Consulter un livre  |")
+        print("|   d: Remetre un livre    |")
+        print("|   e: Quiter              |")
+        print("+--------------------------+")
+        choice = input("Votre choix: ...")
+        match choice.lower:
+            case "a":
+                addbook()
+            case "b":
+                lendbook()
+            case "c":
+                watchBook()
+            case "d":
+                returnBook()
+            case "e":
+                break
+
+
+def addbook():
+    pass
+
+
+def lendbook():
+    pass
+
+
+def watchBook():
+    pass
+
+
+def returnBook():
+    pass
 
 
 def main():
-    f = file("Book.si")
+    f.saveInFile(book1.__str__())
+    f.saveInFile(book2.__str__())
+    f.saveInFile(book3.__str__())
     newbook = f.createOrLoadFile()
-    test = book.__str__()
-    f.saveInFile(test+"\n")
-    print(test)
+    print(newbook)
 
 
 main()
