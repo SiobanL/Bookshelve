d16d497b9f2e6d5f9d1e3963e7816563dd9cc23c
SiobanL <limosanisioban@gmail.com>
2022-11-13 23:18:47 +0100

    maj lendBook()

--- MyClass.py
+++ MyClass.py
@@ -9,34 +9,34 @@ class Book:
         self.bookISBN: int = None
         self.bookID: int = None
         self.bookstate: bool = False
-    
+
     def __str__(self) -> str:
         return str(self.bookTitle) + ";" + str(self.bookAuthor) + ";" + str(self.bookEdition) + ";" + str(self.bookISBN) + ";" + str(self.bookID) + ";" +  str(self.bookstate)+ "\n"
-    
+
     def __repr__(self) -> str:
         return f"The title of book is {self.bookTitle} the author is {self.bookAuthor} and it is part of the edition {self.bookEdition} the ISBN is {self.bookISBN} his state is {self.bookstate}\n"
-    
+
     def get_bookTitle(self)-> str:
         return self.bookTitle
-    
+
     def get_bookAuthor(self)-> str:
         return self.bookAuthor
-    
+
     def get_bookEdition(self)-> str:
         return self.bookEdition
-    
+
     def get_bookISBN(self)-> int:
         return self.bookISBN
-    
+
     def get_bookID(self)-> int:
         return self.bookID
-    
+
     def get_state(self):
-        return self.state
-    
+        return self.bookstate
+
     def changeTitle(self,newTitle:str)-> None:
         self.bookTitle = newTitle
-        
+
     def changeAuthor(self,newAuthor: str)-> None:
         self.bookAuthor = newAuthor
 
@@ -45,46 +45,56 @@ class Book:
 
     def changeISBN(self,newISBN:int)-> None:
         self.bookISBN = newISBN
-    
-    def changeState(self)-> None:
-        self.bookstate = not self.bookstate
-    
-    
+
+    def assignProperty(self,strProperty:list)->None:
+        self.bookTitle = strProperty[0]
+        self.bookAuthor = strProperty[1]
+        self.bookEdition = strProperty[2]
+        self.bookISBN = strProperty[3]
+        self.bookID = strProperty[4]
+        self.bookstate = strProperty[5]
+
+
 class myFile:
     """
         Class to manage db file of books in a typed file as -.si.
     """
     import os.path
-    
+
     def __init__(self,FileName) -> None:
         self.FileName = FileName
 
     def createFile(self)-> None:
         f = open(self.FileName,"x")
         f.close()
-    
-    def saveInFile(self,info:str)-> None:
-        with open(self.FileName,"a+") as f:
-            f.write(info)
-            f.close()
 
     def loadToFile(self)-> list:
-        with open(self.FileName,"r+")as f:
+        with open(self.FileName,"r")as f:
             rlst: list = []
             for line in f:
                 rlst.append(line.split(";"))
-            f.close()
         return rlst
-    
+
     def createOrLoadFile(self)-> list:
         if self.os.path.exists(self.FileName):
             return self.loadToFile()
         else:
             self.createFile()
             return []
-    
-    def modifyLine(self,line:int,item:int):
-        f = open(self.FileName,"w+")
-        f.seek(line)
-        f.write(item)
-        f.close()
+
+    def saveInFile(self,data:str)-> None:
+        with open(self.FileName,"a") as f:
+            f.write(data)
+
+    def listToStr(self,lst:list) -> str:
+        res = ""
+        res = res.join(lst)
+        return res
+
+    def modifyLine(self,i:int,itemToModify:str):
+        with open(self.FileName,"r+") as f:
+            lines = f.readlines()
+            lines[i] = itemToModify
+            with open(self.FileName,"w") as f2:
+                for line in lines:
+                    f2.write(self.listToStr(line))
--- main.py
+++ main.py
@@ -2,15 +2,9 @@ from MyClass import Book, myFile as file
 from os import system as cls
 
 
-# book1 = Book("Title 1","Author 1","Edition 1",11,1)
-# book2 = Book("Title 2","Author 2","Edition 2",22,2)
-# book3 = Book("Title 3","Author 3","Edition 3",33,3)
-
-
-programAuthor: str = "Sioban L." + chr(169)
+# programAuthor: str = "Sioban L." + chr(169)
 f = file("Book.si")
 books = f.loadToFile()
-newBook = Book()
 
 def mainDisplay():
     while True:
@@ -28,7 +22,7 @@ def mainDisplay():
                 addBook()
                 cls("cls")
             case "b":
-                cls("cls")
+                # cls("cls")
                 lendBook()
             case "c":
                 cls("cls")
@@ -43,19 +37,17 @@ def mainDisplay():
 
 
 def addBook():
+    newBook = Book()
+    
     print("+--Ajouter un livre: --+")
     print("|")
-    print("+- Titre: ", end="")
-    newBook.bookTitle = input()
+    newBook.bookTitle = input("+- Titre: ")
     print("|")
-    print("+- Auteur: ", end="")
-    newBook.bookAuthor = input()
+    newBook.bookAuthor = input("+- Auteur: ")
     print("|")
-    print("+- Edition: ", end="")
-    newBook.bookEdition = input()
+    newBook.bookEdition = input("+- Edition: ")
     print("|")
-    print("+- ISNBN: ", end="")
-    newBook.bookISBN = input()
+    newBook.bookISBN = input("+- ISNBN: ")
     print("|")
     print("+----------------------+")
     newBook.bookID = len(books)
@@ -66,22 +58,23 @@ def addBook():
 
 
 def lendBook():
+    currentBook = Book()
+    
     print("+--Preter un livre --+")
     print("|")
     print("+- ID: ",end="")
     search = int(input())
-    book = Book()
-    book.bookTitle = books[search][0]
-    book.bookAuthor = books[search][1]
-    book.bookEdition = books[search][2]
-    book.bookISBN = books[search][3]
-    book.bookID = books[search][4]
-    book.bookstate = books[search][5]
-    book.changeState()
-    books[search] = book.__str__().strip(";")
-    f.modifyLine(search,book.__str__().strip(";"))
-    
-    
+    currentBook.assignProperty(books[search])
+    # good /\
+    #      ||
+    print(not(currentBook.get_state()))
+    print(currentBook.get_state())
+    #      ||
+    # good \/
+    books[search] = currentBook.__str__().split(";")
+    f.modifyLine(search,currentBook.__str__())
+
+
 def watchBook():
     pass
 
@@ -99,6 +92,30 @@ def main():
     pass
 
 
+def writeThreelines():
+    book1 = Book()
+    book1.bookTitle = "Title 1"
+    book1.bookAuthor = "Author 1"
+    book1.bookEdition = "Edition 1"
+    book1.bookISBN = 11
+    book1.bookID = 0
+    f.saveInFile(book1.__str__())
+    book1.bookTitle = "Title 2"
+    book1.bookAuthor = "Author 2"
+    book1.bookEdition = "Edition 2"
+    book1.bookISBN = 22
+    book1.bookID = 1
+    f.saveInFile(book1.__str__())
+    book1.bookTitle = "Title 3"
+    book1.bookAuthor = "Author 3"
+    book1.bookEdition = "Edition 3"
+    book1.bookISBN = 33
+    book1.bookID = 2
+    f.saveInFile(book1.__str__())
+
+
+# writeThreelines()
+# books = f.loadToFile()
 mainDisplay()
 
 # [
@@ -109,3 +126,5 @@ mainDisplay()
 #     'Title 5;Author 5;Edition 5;55;3\n', 
 #     'Title 6;Author 6;Edition 6;66;4\n'
 # ]
+# Title 1;Author 1;Edition 1;11;1;False
+# Title 2;Author 2;Edition 2;22;2;False
